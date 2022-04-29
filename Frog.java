4a3c16c5bdb6f3d4b7c8ec5e5c9058390eb2df68
Anna Cheng <101126737+AnnAapompom@users.noreply.github.com>
2022-04-29 10:41:54 -0400

    Created Frog class

---
+++ Frog.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Frog here.
+ */
+public class Frog extends Actor
+{
+    /**
+     * Act - do whatever the Frog wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(1);// Add your action code here.
+    }
+}
